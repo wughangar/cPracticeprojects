a6b60f3bed0fd8b17a460dfab25e2644e8aea509
wughangar <wughangar.rose@gmail.com>
2023-05-20 11:38:07 +0000

    random number generator that geberates a diffrent number each time

--- numberguess.c
+++ numberguess.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
-
+#include <time.h>
 
 int main()
 {
 	int irandom = 0;
 	int iresponse = 0;
-	irandom = (rand() % 10) + 1;
+	srand(time(NULL));
 
 	while (1)
 	{
+		irandom = (rand() % 10) + 1;
+
 		printf("Enter a number between 1-10 and 0 to exit: ");
 		scanf("%d", &iresponse);
 
