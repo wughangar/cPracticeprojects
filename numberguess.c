545f30de97ba8d4e54238d72a23b27596d459536
wughangar <wughangar.rose@gmail.com>
2023-05-20 11:24:44 +0000

    simple random number generator

---
+++ numberguess.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	int irandom = 0;
+	int iresponse = 0;
+	irandom = (rand() % 10) + 1;
+
+	while (1)
+	{
+		printf("Enter a number between 1-10 and 0 to exit: ");
+		scanf("%d", &iresponse);
+
+		if isdigit(iresponse == 0)
+		{
+			printf("Please enter a valid number\n");
+		}
+		if (iresponse == irandom)
+		{
+			printf("That is correct!\n");
+		}
+		if (iresponse == 0)
+		{
+			exit(0);
+		}
+		else
+		{
+			printf("Wrong, the random number is:%d\n", irandom);
+		}
+	}
+	return (0);
+}
